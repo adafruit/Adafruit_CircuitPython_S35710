50e7b2415bc363ce77a400c3a64a3dc384453203
Liz <liz@adafruit.com>
2024-05-09 14:09:53 -0400

    cookiecutter

---
+++ adafruit_s35710.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 Liz Clark for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+"""
+`adafruit_s35710`
+================================================================================
+
+CircuitPython driver for the S-35710 low-power wake up timer
+
+
+* Author(s): Liz Clark
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+* `Adafruit S-35710 Low-Power Wake Up Timer Breakout <https://www.adafruit.com/product/5959>`_
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_S35710.git"
